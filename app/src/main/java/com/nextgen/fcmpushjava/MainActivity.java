61bff56199b6df7391704c09da5545e558634266
tongsu <tong_su@hotmail.com>
2022-09-24 23:00:15 -0400

    display the device registration token to be used by FCM test message

--- app/src/main/java/com/nextgen/fcmpushjava/MainActivity.java
+++ app/src/main/java/com/nextgen/fcmpushjava/MainActivity.java
@@ -1,14 +1,46 @@
 package com.nextgen.fcmpushjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etToken = findViewById(R.id.etToken);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed");
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        String msg = "device token: " + token.toString();
+                        System.out.println(msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        etToken.setText(token);
+                    }
+                });
     }
 }
